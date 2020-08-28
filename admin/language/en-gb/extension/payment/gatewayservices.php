138dffedb11a2968177186a1afecb1a5f756fb05
E E <C:\Users\itman\AppData\Roaming\The Bat!>
2020-08-28 17:10:31 +0300

    fix spacing

--- admin/language/en-gb/extension/payment/gatewayservices.php
+++ admin/language/en-gb/extension/payment/gatewayservices.php
@@ -1,36 +1,36 @@
 <?php
 // Heading
-$_['heading_title']                  = 'GatewayServices Redirect';
+$_['heading_title']              = 'GatewayServices Redirect';
 
 // Text
-$_['text_extension']                 = 'Extensions';
-$_['text_success']                   = 'Success: You have modified GatewayServices account details!';
-$_['text_edit']                      = 'Edit GatewayServices Redirect';
-$_['text_gatewayservices']                    = '<a target="_BLANK" href="https://gateway-services.com"><img src="view/image/payment/gatewayservices.png" alt="GatewayServices Redirect Website" title="GatewayServices Redirect Website" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_live']                      = 'Live';
-$_['text_sandbox']                   = 'Sandbox';
+$_['text_extension']             = 'Extensions';
+$_['text_success']               = 'Success: You have modified GatewayServices account details!';
+$_['text_edit']                  = 'Edit GatewayServices Redirect';
+$_['text_gatewayservices']       = '<a target="_BLANK" href="https://gateway-services.com"><img src="view/image/payment/gatewayservices.png" alt="GatewayServices Redirect Website" title="GatewayServices Redirect Website" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_live']                  = 'Live';
+$_['text_sandbox']               = 'Sandbox';
 
 // Entry
-$_['entry_merchant_id']                  = 'Merchant ID';
-$_['entry_terminal_id']                  = 'Terminal ID';
-$_['entry_api_password']                 = 'API Password';
-$_['entry_private_key']                  = 'Merchant Private Key';
-$_['entry_transaction_type']             = 'Transaction Type';
-$_['entry_api_password']                 = 'GatewayServices Public Key';
-$_['entry_test']                         = 'Test mode';
-$_['entry_total']                        = 'Total';
-$_['entry_order_status']                 = 'Completed Status';
-$_['entry_geo_zone']                     = 'Geo Zone';
-$_['entry_status']                       = 'Status';
-$_['entry_sort_order']                   = 'Sort Order';
+$_['entry_merchant_id']          = 'Merchant ID';
+$_['entry_terminal_id']          = 'Terminal ID';
+$_['entry_api_password']         = 'API Password';
+$_['entry_private_key']          = 'Merchant Private Key';
+$_['entry_transaction_type']     = 'Transaction Type';
+$_['entry_api_password']         = 'GatewayServices Public Key';
+$_['entry_test']                 = 'Test mode';
+$_['entry_total']                = 'Total';
+$_['entry_order_status']         = 'Completed Status';
+$_['entry_geo_zone']             = 'Geo Zone';
+$_['entry_status']               = 'Status';
+$_['entry_sort_order']           = 'Sort Order';
 
 // Help
-$_['help_total']                         = 'The checkout total the order must reach before this payment method becomes active';
-$_['help_gatewayservices_setup']         = '<a target="_blank" href="http://www.opencart.cn/docs/gatewayservices">Click here</a> to learn how to set up GatewayServices account.';
+$_['help_total']                 = 'The checkout total the order must reach before this payment method becomes active';
+$_['help_gatewayservices_setup'] = '<a target="_blank" href="http://www.opencart.cn/docs/gatewayservices">Click here</a> to learn how to set up GatewayServices account.';
 
 // Error
-$_['error_permission']                    = 'Warning: You do not have permission to modify payment GatewayServices!';
-$_['error_merchant_id']                   = 'Merchant ID required!';
-$_['error_terminal_id']                   = 'Terminal ID required!';
-$_['error_transaction_type']              = 'Merchant Transaction Type required!';
-$_['error_api_password']                  = 'API Password required!';
+$_['error_permission']           = 'Warning: You do not have permission to modify payment GatewayServices!';
+$_['error_merchant_id']          = 'Merchant ID required!';
+$_['error_terminal_id']          = 'Terminal ID required!';
+$_['error_transaction_type']     = 'Merchant Transaction Type required!';
+$_['error_api_password']         = 'API Password required!';
